f3dd6bab20f11473f128574eabbf106a09f4db70
akbel <colakakbel@gmail.com>
2020-01-22 13:20:47 +0100

    word sql solutions (not sure 2 of them)

---
+++ world.sql solutions week-2.sql
@@ -0,0 +1,35 @@
+use world;
+-- select * from country where name = 'india';
+select name, population from city where countrycode = 'IND' order by population desc limit 1;
+-- select max(population) from city where countrycode = 'IND'; SHOWS ONLY POPULATION
+-- select name, max(population) from city where CountryCode = 'IND'; THIS DOES NOT WORK
+-- select name, max(population) from city where CountryCode = 'IND' group by name; THIS SHOWS ALL THE CITIES
+
+-- second statement:
+-- select name, population from country where population =(select population from city where population >= 3000000);
+-- select city.population, country.name from country inner join city on country.code = city.CountryCode; 
+-- select country.name from country inner join city on country.code = city.CountryCode where city.population >= 3000000;
+select count(country.name) as CrowdedCities, country.name
+from country inner join city
+on country.code = city.CountryCode
+where city.population >= 3000000
+group by country.name
+having CrowdedCities > 3;
+
+select c.name, count(l.Language)
+from country as c inner join countrylanguage as l
+on c.code = l.CountryCode
+where IsOfficial = 'T'
+group by c.name; -- FIRST JOIN THAN ADD THE COUNT FUNC.
+
+select c.name, count(l.Language) as officialLanguage
+from country as c inner join countrylanguage as l
+on c.code = l.CountryCode
+where IsOfficial = 'T'
+group by c.name
+having officialLanguage = '1';
+
+select name, capital
+from country
+where capital is null;
+
