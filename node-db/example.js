cf966f936c596675d85829ef4cd9302350a221c4
akbel <colakakbel@gmail.com>
2020-01-31 13:43:58 +0100

    fs, read,write and append

--- node-db/example.js
+++ node-db/example.js
@@ -4,7 +4,12 @@ var string = require('./string')
 var sayHello = require('./function')
 
 http.createServer(function(request,response){
-    response.end('hello')
+    if(request.url == '/admin'){
+        response.write('you are admin, this is your page.\n')
+    } else{
+        response.write('you are not admin, so you came to home page.\n')
+    }
+    response.end('end of the response')
 }).listen(8080)
 
 log.information('my log module is on')
---
+++ node-db/fileStream.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+fs.readFile('file.txt', function(error,data){
+    if(!!error){ throw error};
+    console.log(data.toString());
+});
+
+fs.writeFile('file.txt', 'I want to write something on my text file. and this changed my original text.\n',function(error){
+    if(!!error){ throw error};
+    console.log('you wrote to text file :\n');
+});
+
+fs.appendFile('file.txt', 'now I am adding some text to existing one.\n',function(error){
+    if(!!error){ throw error};
+    console.log('you add to text file :\n');
+});
